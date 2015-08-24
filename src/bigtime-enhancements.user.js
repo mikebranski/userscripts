f078975c67218a9e5f63506009eab6fe5021a20c
Mike Branski <mike.branski@7summitsinc.com>
2015-08-24 09:54:40 -0500

    More friendly name format

--- src/bigtime-enhancements.user.js
+++ src/bigtime-enhancements.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         bigtime-enhancements
+// @name         BigTime Enhancements
 // @namespace    http://mikebranski.com/
-// @version      2.1.2
+// @version      2.1.3
 // @description  Adds keyboard shortcuts and displays a running weekly total for each project.
 // @author       Mike Branski (@mikebranski)
 // @match        https://*.bthost.com/*
@@ -11,14 +11,14 @@
 // ==/UserScript==
 
 (function(window, undefined) {
-	
+
 	WeeklyTotalsCalculator = {
-		
+
 		// An array of all external script dependencies to be loaded.
 		dependencies: [
 			'https://cdnjs.cloudflare.com/ajax/libs/mousetrap/1.4.6/mousetrap.min.js'
 		],
-		
+
 		dependencies_to_load: [],
 
 		timesheet: document.getElementById('tblGrid'),
@@ -28,43 +28,43 @@
 		init: function() {
 			// Load external resources.
 			this.fetchDependencies();
-			
+
 			// Run the initial calculation.
 			this.calculateTotals();
 
 			// Bind event listeners.
 			this.bindEventListeners();
 		},
-		
+
 		fetchDependencies: function() {
 			// Identify the injection point for the scripts.
 			this.script_target = document.getElementsByTagName('head')[0];
-			
+
 			// Copy the depencies property to a mutable array we'll cycle through.
 			this.dependencies_to_load = this.dependencies;
-			
+
 			// Kick over the first domino.
 			this.loadNextDependency();
 		},
-		
+
 		loadNextDependency: function() {
 			var self, script, url;
-			
+
 			// Safety net.
 			if (!this.dependencies_to_load.length) {
 				return;
 			}
-			
+
 			// Needed for the onload callback.
 			self = this;
-			
+
 			// Shift the next dependency off the chain for loading.
 			url = this.dependencies_to_load.shift();
 
 			// Create the script element we're going to inject.
 			script = document.createElement('script');
 			script.src = url;
-			
+
 			// Bind the callback to fire once the script has loaded.
 			script.onload = function() {
 				// If we've finished loading dependencies, let the app know.
@@ -72,91 +72,91 @@
 					self.dependenciesLoaded();
 					return;
 				}
-				
+
 				// Otherwise, load the next dependency.
 				self.loadNextDependency();
 			}
-			
+
 			// Insert the script.
 			this.script_target.appendChild(script);
 		},
-		
+
 		/**
-		 * This runs when all dependencies have finished loading, typically used 
+		 * This runs when all dependencies have finished loading, typically used
 		 * to set up additional app functionality and event bindings.
 		 */
 		dependenciesLoaded: function() {
-			
+
 			// Override the stopCallback function so that it always returns false.
 			// This ensures Mousetrap fires even when the user is in text fields
 			// and other content editable elements.
 			Mousetrap.stopCallback = function(e, element, combo) {
 				return false;
 			}
-			
+
 			// Go to the current week's timesheet when Cmd + Shift + C is pressed.
 			Mousetrap.bind('command+shift+c', function(e) {
 				var submission_uri;
-				
+
 				// @todo: Create a URL generator function so we don't need to
 				//        do this parsing silliness every time.
-				
+
 				// Build the URI to the current week's timesheet view.
 				current_timesheet_uri = window.location.pathname;
-				
-				// If the URI begins with a slash, remove it for now to make 
+
+				// If the URI begins with a slash, remove it for now to make
 				// splitting on slashes easier.
 				if (current_timesheet_uri.substring(0, 1) === '/') {
 					current_timesheet_uri = current_timesheet_uri.substring(1);
 				}
-				
-				// Determine the account URL identifier: split on slashes and grab 
+
+				// Determine the account URL identifier: split on slashes and grab
 				// the first part, and add a leading slash.
 				current_timesheet_uri = '/' + current_timesheet_uri.split('/')[0];
-				
+
 				// Tack on the current week's timesheet page.
 				current_timesheet_uri += '/EAPSA_MGMT.ASP?WCI=eaMain&WCE=tplTableDef&HTML=TSWKEI.htm&ObjectType=EATimesheet';
-				
+
 				// And away we go!
 				window.location.href = current_timesheet_uri;
-				
+
 				// Flerp flerp.
 				return false;
 			});
-			
+
 			// Save the timesheet when Cmd + S is pressed.
 			Mousetrap.bind('command+s', function(e) {
 				var save_btn = document.querySelector('img[src="images/buttons/save_sm.gif"]');
-				
+
 				save_btn.click();
-				
+
 				return false;
 			});
-			
+
 			// Go to the timesheet submission page when Cmd + Shift + S is pressed.
 			Mousetrap.bind('command+shift+s', function(e) {
 				var submission_uri;
-				
-				// Build the URI we'll send the user to where they can submit their 
+
+				// Build the URI we'll send the user to where they can submit their
 				// timesheet.
 				submission_uri = window.location.pathname;
-				
-				// If the URI begins with a slash, remove it for now to make 
+
+				// If the URI begins with a slash, remove it for now to make
 				// splitting on slashes easier.
 				if (submission_uri.substring(0, 1) === '/') {
 					submission_uri = submission_uri.substring(1);
 				}
-				
-				// Determine the account URL identifier: split on slashes and grab 
+
+				// Determine the account URL identifier: split on slashes and grab
 				// the first part, and add a leading slash.
 				submission_uri = '/' + submission_uri.split('/')[0];
-				
+
 				// Tack on the timesheet submission page.
 				submission_uri += '/EAPSA_MGMT.ASP?WCI=eaMAIN&WCE=tplBasic&HTML=Daily_511.htm';
-				
+
 				// And away we go!
 				window.location.href = submission_uri;
-				
+
 				// Flerp flerp.
 				return false;
 			});
@@ -229,5 +229,5 @@
 	};
 
 	WeeklyTotalsCalculator.init();
-	
+
 })(window);
